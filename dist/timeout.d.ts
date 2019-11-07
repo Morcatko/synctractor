0d2ff43bb66453aeb22899c9fe7f5e036923823e
Ondra <ondrej.morsky@hotmail.com>
2019-11-07 08:13:53 +0100

    trackBackgroundOp + tags

--- dist/timeout.d.ts
+++ dist/timeout.d.ts
@@ -1,8 +1,8 @@
 declare type FilterFunc = (handler: Function, timeout?: number, ...args: any[]) => boolean;
 declare function _monitoredSetTimeout(handler: Function, timeout?: number, ...args: any[]): number;
 declare function _monitoredClearTimeout(handle: number): void;
-export declare const nativeSetTimeout: (handler: TimerHandler, timeout?: number, ...arguments: any[]) => number;
-export declare const nativeClearTimeout: (handle?: number) => void;
+export declare const nativeSetTimeout: ((handler: TimerHandler, timeout?: number, ...arguments: any[]) => number) & ((handler: TimerHandler, timeout?: number, ...arguments: any[]) => number);
+export declare const nativeClearTimeout: ((handle?: number) => void) & ((handle?: number) => void);
 export declare const setTimeout: typeof _monitoredSetTimeout;
 export declare const clearTimeout: typeof _monitoredClearTimeout;
 export declare const monitorTimeout: (filter?: FilterFunc) => void;
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,15 @@
+import { startBackgroundOp, endBackgroundOp } from './testability';
+
 export * from './testability';
 export * from './fetch';
 export * from './timeout';
+
+export async function trackBackgroundOp<T>(action: () => Promise<T>): Promise<T> {
+    startBackgroundOp();
+    try {
+        return await action()
+    }
+    finally {
+        endBackgroundOp();
+    }
+}
